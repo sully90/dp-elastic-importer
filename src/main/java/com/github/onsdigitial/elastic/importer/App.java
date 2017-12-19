20411d74306294990495ce3ea9ebe8fead62a219
David Sullivan <dsullivan0190@gmail.com>
2017-12-19 14:24:58 +0000

    Switched to OpenNlpSearchClient and Non-default Bulk configuration

--- src/main/java/com/github/onsdigitial/elastic/importer/App.java
+++ src/main/java/com/github/onsdigitial/elastic/importer/App.java
@@ -7,10 +7,13 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.onsdigital.elasticutils.client.Host;
 import com.github.onsdigital.elasticutils.client.bulk.configuration.BulkProcessorConfiguration;
+import com.github.onsdigital.elasticutils.client.bulk.options.BulkProcessingOptions;
 import com.github.onsdigital.elasticutils.client.generic.ElasticSearchClient;
-import com.github.onsdigital.elasticutils.client.generic.TransportSearchClient;
+import com.github.onsdigital.elasticutils.client.generic.RestSearchClient;
+import com.github.onsdigital.elasticutils.client.http.SimpleRestClient;
 import com.github.onsdigital.elasticutils.client.type.DefaultDocumentTypes;
 import com.github.onsdigital.elasticutils.util.ElasticSearchHelper;
+import com.github.onsdigitial.elastic.importer.elasticsearch.OpenNlpSearchClient;
 import com.github.onsdigitial.elastic.importer.models.page.adhoc.AdHoc;
 import com.github.onsdigitial.elastic.importer.models.page.base.Page;
 import com.github.onsdigitial.elastic.importer.models.page.base.PageType;
@@ -43,8 +46,12 @@ import com.github.onsdigitial.elastic.importer.models.page.taxonomy.TaxonomyLand
 import com.github.onsdigitial.elastic.importer.models.page.visualisation.Visualisation;
 import com.github.onsdigitial.elastic.importer.util.FileScanner;
 import org.apache.commons.io.IOUtils;
+import org.elasticsearch.action.bulk.BackoffPolicy;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.ByteSizeUnit;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -215,7 +222,7 @@ public class App {
 
     public void index() throws IOException {
         List<File> files = this.fileScanner.getFiles();
-        try (ElasticSearchClient<Page> searchClient = getClient(this.host)) {
+        try (ElasticSearchClient<Page> searchClient = getClientTcp(this.host)) {
 
             // Reset the index
             resetIndex(this.indexName, searchClient);
@@ -236,24 +243,63 @@ public class App {
             searchClient.dropIndex(indexName);
         }
         Settings settings = ElasticSearchHelper.loadSettingsFromFile("/search/", "index-config.yml");
-        Map<String, Object> mapping = ElasticSearchHelper.loadMappingFromFile("/search", "default-document-mapping.json");
+        Map<String, Object> mapping = ElasticSearchHelper.loadMappingFromFile("/search/", "default-document-mapping.json");
         searchClient.createIndex(indexName, DefaultDocumentTypes.DOCUMENT, settings, mapping);
     }
 
-    private static ElasticSearchClient<Page> getClient(Host host) throws UnknownHostException {
+    private static ElasticSearchClient<Page> getClientTcp(Host host) throws UnknownHostException {
         TransportClient transportClient = ElasticSearchHelper.getTransportClient(host);
-        BulkProcessorConfiguration configuration = ElasticSearchHelper.getDefaultBulkProcessorConfiguration();
-        return new TransportSearchClient<>(transportClient, configuration);
+        return new OpenNlpSearchClient<>(transportClient, getConfiguration());
+    }
+
+    private static ElasticSearchClient<Page> getClientHttp(Host host) throws UnknownHostException {
+        SimpleRestClient restClient = ElasticSearchHelper.getRestClient(host);
+        return new RestSearchClient<>(restClient, getConfiguration());
+    }
+
+        private static BulkProcessorConfiguration getConfiguration() {
+        BulkProcessorConfiguration bulkProcessorConfiguration = new BulkProcessorConfiguration(BulkProcessingOptions.builder()
+                .setBulkActions(100)
+                .setBulkSize(new ByteSizeValue(5, ByteSizeUnit.MB))
+                .setFlushInterval(TimeValue.timeValueSeconds(5))
+                .setConcurrentRequests(8)
+                .setBackoffPolicy(
+                        BackoffPolicy.exponentialBackoff(TimeValue.timeValueMillis(1000), 5))
+                .build());
+        return bulkProcessorConfiguration;
     }
 
     public static void main(String[] args) {
         App app = new App();
 
         try {
+            long startTime = System.currentTimeMillis();
             app.index();
+            long endTime = System.currentTimeMillis();
+            long duration = (endTime - startTime);
+
+            System.out.format("Milli = %s, ( S_Start : %s, S_End : %s ) \n", duration, startTime, endTime );
+            System.out.println("Human-Readable format : "+ millisToShortDHMS( duration ) );
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static String millisToShortDHMS(long duration) {
+        String res = "";    // java.util.concurrent.TimeUnit;
+        long days       = TimeUnit.MILLISECONDS.toDays(duration);
+        long hours      = TimeUnit.MILLISECONDS.toHours(duration) -
+                TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(duration));
+        long minutes    = TimeUnit.MILLISECONDS.toMinutes(duration) -
+                TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(duration));
+        long seconds    = TimeUnit.MILLISECONDS.toSeconds(duration) -
+                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration));
+        long millis     = TimeUnit.MILLISECONDS.toMillis(duration) -
+                TimeUnit.SECONDS.toMillis(TimeUnit.MILLISECONDS.toSeconds(duration));
+
+        if (days == 0)      res = String.format("%02d:%02d:%02d.%04d", hours, minutes, seconds, millis);
+        else                res = String.format("%dd %02d:%02d:%02d.%04d", days, hours, minutes, seconds, millis);
+        return res;
     }
 
 }
